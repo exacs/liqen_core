2965c809a95e3fdda39bfabeec6b081d708fc9bc
Carlos Saito <c.saito.style@gmail.com>
2017-08-16 09:43:29 +0200

    Fix Travis configuration

--- config/test.travis.exs
+++ config/test.travis.exs
@@ -2,7 +2,7 @@ use Mix.Config
 
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
-config :core, LiqenCore.Endpoint,
+config :liqen_core, LiqenCore.Endpoint,
   http: [port: 4001],
   server: false
 
@@ -10,7 +10,7 @@ config :core, LiqenCore.Endpoint,
 config :logger, level: :warn
 
 # Configure your database
-config :core, LiqenCore.Repo,
+config :liqen_core, LiqenCore.Repo,
   adapter: Ecto.Adapters.Postgres,
   username: "postgres",
   password: "",
